8dd774342cc35ec01e63cb2d83fe461b063452bc
Tapiwa Makandigona <tapiwamakandigona@users.noreply.github.com>
2026-01-02 17:13:01 +0200

    Switch to BrowserRouter for Clean URLs & Fix 404 Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import { Landing } from './pages/Landing'
 import { Login } from './pages/Login'
@@ -87,11 +87,11 @@ function AppRoutes() {
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
